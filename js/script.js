9b7196fd906c4a921c8af9faed37f485eb77fbb2
Joacodef <joacodef1@gmail.com>
2022-06-05 20:37:41 -0400

    grafico de muestra, llamada a api de bitcoin

--- js/script.js
+++ js/script.js
@@ -4,3 +4,47 @@ function actualizarReloj( ) {
 	document.getElementById("reloj").textContent = hora;
 	setTimeout("actualizarReloj()", 1000);
 }
+
+async function llamarAPI(){
+	let response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+	let data = await response.text(); 
+	var obj = JSON.parse(data);
+	var precioBC = parseInt(parseFloat(String(obj.bpi.USD.rate).replace(",",""))*811.81);
+	
+	var xyValues = [
+	  {x:50, y:7},
+	  {x:60, y:8},
+	  {x:70, y:8},
+	  {x:80, y:9},
+	  {x:90, y:9},
+	  {x:100, y:9},
+	  {x:110, y:10},
+	  {x:120, y:11},
+	  {x:130, y:14},
+	  {x:140, y:14},
+	  {x:150, y:15}
+	];
+
+	new Chart("myChart", {
+	  type: "scatter",
+	  data: {
+	    datasets: [{
+	      pointRadius: 4,
+	      pointBackgroundColor: "rgba(0,0,255,1)",
+	      data: xyValues
+	    }]
+	  }
+	});
+
+	
+	document.getElementById("precioBC").textContent = 
+		"Precio Bitcoin: $"+numberWithCommas(precioBC);
+
+
+	setTimeout("llamarAPI()", 60000);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
